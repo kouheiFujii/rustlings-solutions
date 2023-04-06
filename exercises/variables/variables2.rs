dbaa2b43284759b472b52dc6fe6812fcfcf8f345
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-04-07 08:45:52 +0900

    Data type.

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,10 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    // Data type
+    // https://doc.rust-lang.org/book/ch03-02-data-types.html#data-types
+    let x: i8 = 10;
     if x == 10 {
         println!("x is ten!");
     } else {
