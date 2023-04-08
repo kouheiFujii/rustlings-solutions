b21667051aa80038b5fb50762a15d105cbbc36e8
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-04-08 19:57:35 +0900

    tuple 基本

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -2,11 +2,11 @@
 // Destructure the `cat` tuple so that the println will work.
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    // タプル: 複数の型をまとめて扱うことができる
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    // 他にも cat.0, cat.1 というようにアクセスできる
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
